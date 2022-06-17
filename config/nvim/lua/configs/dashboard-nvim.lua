6913f077252e46c837755eeb50d5de36d86e5a35
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2022-06-17 17:52:44 +0200

    Update dashboard

--- config/nvim/lua/configs/dashboard-nvim.lua
+++ config/nvim/lua/configs/dashboard-nvim.lua
@@ -1,46 +1,69 @@
 local M = {}
 
 function M.config()
-	local g = vim.g
-	local fn = vim.fn
+	local status_ok, db = pcall(require, "dashboard")
 
-	local plugins_count = fn.len(vim.fn.globpath(fn.stdpath("data") .. "/site/pack/packer/start", "*", 0, 1))
+	if status_ok then
+		db.custom_header = {
+			" ",
+			" ",
+			" ",
+			" ██████   ██████  ███████ ██   ██ ███████ ██    ██ ████████ ███████  ██████  ██    ██",
+			"██    ██ ██    ██ ██      ██   ██ ██      ██    ██    ██    ██      ██    ██ ██    ██",
+			"██ ██ ██ ██    ██ ███████ ███████ █████   ██    ██    ██    ███████ ██    ██ ██    ██",
+			"██ ██ ██ ██    ██      ██ ██   ██ ██       ██  ██     ██         ██ ██    ██  ██  ██",
+			" █ ████   ██████  ███████ ██   ██ ███████   ████      ██    ███████  ██████    ████",
+			" ",
+			" ",
+			" ",
+			" ",
+		}
 
-	g.dashboard_disable_statusline = 1
-	g.dashboard_default_executive = "telescope"
-	g.dashboard_custom_header = {
-		" ",
-		" ",
-		" ",
-		" ",
-		" ",
-		" ██████   ██████  ███████ ██   ██ ███████ ██    ██ ████████ ███████  ██████  ██    ██",
-		"██    ██ ██    ██ ██      ██   ██ ██      ██    ██    ██    ██      ██    ██ ██    ██",
-		"██ ██ ██ ██    ██ ███████ ███████ █████   ██    ██    ██    ███████ ██    ██ ██    ██",
-		"██ ██ ██ ██    ██      ██ ██   ██ ██       ██  ██     ██         ██ ██    ██  ██  ██",
-		" █ ████   ██████  ███████ ██   ██ ███████   ████      ██    ███████  ██████    ████",
-		" ",
-		" ",
-		" ",
-	}
-
-	g.dashboard_custom_section = {
-		a = {
-			description = { "   Open dotfiles             SPC f d" },
-			command = "lua require('telescope.builtin').find_files({prompt_title='Dotfiles', cwd='$HOME/.dotfiles'})",
-		},
-		b = { description = { "   Find File                 SPC f f" }, command = "Telescope find_files" },
-		c = { description = { "   Recents                   SPC f o" }, command = "Telescope oldfiles" },
-		d = { description = { "   Find Word                 SPC f g" }, command = "Telescope live_grep" },
-		e = { description = { "   New File                  SPC f n" }, command = "DashboardNewFile" },
-		f = { description = { "   Bookmarks                 SPC f m" }, command = "Telescope marks" },
-		g = { description = { "   Last Session              SPC s l" }, command = "SessionLoad" },
-	}
-
-	g.dashboard_custom_footer = {
-		" ",
-		" Loaded " .. plugins_count .. " plugins ",
-	}
+		db.custom_center = {
+			{
+				icon = "  ",
+				desc = "Open dotfiles             ",
+				shortcut = "SPC f d",
+				action = "lua require('telescope.builtin').find_files({prompt_title='Dotfiles', cwd='$HOME/.dotfiles'})",
+			},
+			{
+				icon = "  ",
+				desc = "Find File                 ",
+				shortcut = "SPC f f",
+				action = "Telescope find_files",
+			},
+			{
+				icon = "  ",
+				desc = "Recents                   ",
+				shortcut = "SPC f o",
+				action = "Telescope oldfiles",
+			},
+			{
+				icon = "  ",
+				desc = "Find Word                 ",
+				shortcut = "SPC f g",
+				action = "Telescope live_grep",
+			},
+			{
+				icon = "  ",
+				desc = "New File                  ",
+				shortcut = "SPC f n",
+				action = "DashboardNewFile",
+			},
+			{
+				icon = "  ",
+				desc = "Bookmarks                 ",
+				shortcut = "SPC f m",
+				action = "Telescope marks",
+			},
+			{
+				icon = "  ",
+				desc = "Last Session              ",
+				shortcut = "SPC s l",
+				action = "SessionLoad",
+			},
+		}
+	end
 end
 
 return M
