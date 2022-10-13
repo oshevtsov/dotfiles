22428f35bbae7b567669d7e5009e7e403922dd7a
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2022-10-13 11:24:26 +0200

    Update lua lsp config

--- config/nvim/lua/configs/lsp/server-settings/sumneko_lua.lua
+++ config/nvim/lua/configs/lsp/server-settings/sumneko_lua.lua
@@ -13,8 +13,12 @@ return {
       },
       workspace = {
         library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
+          vim.fn.expand("$VIMRUNTIME/lua"),
+          vim.fn.stdpath("config") .. "/lua",
+        },
+        ignoreDire = {
+          vim.fn.stdpath("config") .. "/.undo",
+          vim.fn.stdpath("config") .. "/.swp",
         },
       },
     },
