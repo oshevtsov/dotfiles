084a841439fb251008f61c35ccbd32ed95f37f5d
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2022-12-26 11:45:43 +0100

    Add comment with alternative colorscheme to try

--- config/nvim/lua/core/defaults.lua
+++ config/nvim/lua/core/defaults.lua
@@ -1,5 +1,6 @@
 local defaults = {
 	colorscheme = {
+		-- if you get bored, try https://github.com/marko-cerovac/material.nvim
 		repo = "EdenEast/nightfox.nvim",
 		name = "nightfox",
 		theme = "nightfox",
