a4ce4a03e818f7f0dc7cac6580748e7b4a02c2ee
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2023-03-09 10:24:03 +0100

    Update dependencies and yamlls config

--- config/nvim/lua/configs/lsp/server-settings/yamlls.lua
+++ config/nvim/lua/configs/lsp/server-settings/yamlls.lua
@@ -1,14 +1,11 @@
 return {
-	settings = {
-		yaml = {
-			schemaStore = {
-				url = "https://www.schemastore.org/api/json/catalog.json",
-				enable = true,
-			},
-			schemas = {
-				["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.json"] = "openapi/**/*.yaml",
-			},
-			completion = true,
-		},
-	},
+  settings = {
+    yaml = {
+      schemaStore = {
+        url = "https://www.schemastore.org/api/json/catalog.json",
+        enable = true,
+      },
+      completion = true,
+    },
+  },
 }
