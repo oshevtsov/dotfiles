4f798fcb5a36c5606e4f0997fa46d8d0380ae0a0
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2024-03-17 19:03:33 +0100

    Add neo-tree mapping to copy file paths variations

--- config/nvim/lua/configs/neo-tree.lua
+++ config/nvim/lua/configs/neo-tree.lua
@@ -33,10 +33,54 @@ function M.config()
           },
         },
       },
+      -- custom commands and/or command overrides
+      -- see https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370#discussioncomment-6679447
+      commands = {
+        copy_selector = function(state)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
+
+          local vals = {
+            ["BASENAME"] = modify(filename, ":r"),
+            ["EXTENSION"] = modify(filename, ":e"),
+            ["FILENAME"] = filename,
+            ["PATH (CWD)"] = modify(filepath, ":."),
+            ["PATH (HOME)"] = modify(filepath, ":~"),
+            ["PATH"] = filepath,
+            ["URI"] = vim.uri_from_fname(filepath),
+          }
+
+          -- filter out empty values
+          local options = vim.tbl_filter(function(val)
+            return vals[val] ~= ""
+          end, vim.tbl_keys(vals))
+
+          if vim.tbl_isempty(options) then
+            vim.notify("No values to copy", vim.log.levels.WARN)
+            return
+          end
+
+          table.sort(options)
+          vim.ui.select(options, {
+            prompt = "Choose to copy to clipboard:",
+            format_item = function(item)
+              return ("%s: %s"):format(item, vals[item])
+            end,
+          }, function(choice)
+            local result = vals[choice]
+            if result then
+              vim.fn.setreg("+", result)
+            end
+          end)
+        end,
+      },
       window = {
         width = 33,
         mappings = {
           ["o"] = "open",
+          ["Y"] = "copy_selector",
         },
       },
       filesystem = {
