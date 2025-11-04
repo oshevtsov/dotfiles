9375a5d250646faabf61aa8294e2f135f8696cce
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2025-11-04 08:42:26 +0100

    Remove McpHub

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -1076,20 +1076,4 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
-
-  -- MCP Hub
-  {
-    "ravitemer/mcphub.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    build = "bundled_build.lua", -- Bundles `mcp-hub` binary along with the neovim plugin
-    config = function()
-      require("mcphub").setup({
-        use_bundled_binary = true, -- Use local `mcp-hub` binary
-      })
-    end,
-  },
-
-  -- AI companion
 }
