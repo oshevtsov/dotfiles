98fe93ef92fd0d288af4dc765ac50593a41918da
Oleksii Shevtsov <oleksii.shevtsov@Oleksiis-MacBook-Pro.local>
2026-04-10 09:52:25 +0200

    Replace Comment.nvim with mini-comment

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -17,24 +17,6 @@ return {
     end,
   },
 
-  -- Commenting
-  {
-    "numToStr/Comment.nvim",
-    dependencies = {
-      {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        config = function()
-          require("ts_context_commentstring").setup({ enable_autocmd = false })
-        end,
-      },
-    },
-    config = function()
-      require("Comment").setup({
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      })
-    end,
-  },
-
   -- Smooth escaping
   {
     "max397574/better-escape.nvim",
--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -1,7 +1,22 @@
 return {
   "nvim-mini/mini.nvim",
+  dependencies = {
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      config = function()
+        require("ts_context_commentstring").setup({ enable_autocmd = false })
+      end,
+    },
+  },
   version = false,
   config = function()
     require("mini.splitjoin").setup()
+    require("mini.comment").setup({
+      options = {
+        custom_commentstring = function()
+          return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    })
   end,
 }
